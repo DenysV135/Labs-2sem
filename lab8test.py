8db6cb0e7e1b24d49dddb32b62459fb96c99a39a
DenysV135 <s9y4zbt7jh@privaterelay.appleid.com>
2026-05-30 21:54:38 +0300

    Update lab8test.py
    Correcting test logic

--- lab8test.py
+++ lab8test.py
@@ -3,7 +3,7 @@ from src.electricians import calculate_max_wire_length
 
 class TestElectricians(unittest.TestCase):
     def test_example_1(self):
-        self.assertAlmostEqual(calculate_max_wire_length(2, [3, 3, 3]), 5.65, places=2)
+        self.assertAlmostEqual(calculate_max_wire_length(2, [3, 3, 3]), 5.66, places=2)
 
     def test_example_2(self):
         self.assertAlmostEqual(calculate_max_wire_length(100, [1, 1, 1, 1]), 300.00, places=2)
