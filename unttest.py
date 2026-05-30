4e4629f196f0aa93cbbbf58a03f3ab2c10419505
DenysV135 <s9y4zbt7jh@privaterelay.appleid.com>
2026-05-30 16:35:38 +0300

    Update unttest.py
    Fixing unittest logic by adding "assertequal", comparing result, to upfront known one

--- unttest.py
+++ unttest.py
@@ -3,16 +3,16 @@ import unittest
 
 class test(unittest.TestCase):
     def test_alg(self):
-        calcut(2,3,4)
+        self.assertEqual(calcut(2, 3, 4), 6)
         
     def test_alll(self):
-        calcut(10,2,3)
+        self.assertEqual(calcut(10, 2, 3), 9)
 
     def test_sss(self):
-        calcut(2, 1000000000, 999999999)
+        self.assertEqual(calcut(2, 1000000000, 999999999), 1999999998)
 
     def test_ggg(self):
-        calcut(4, 1, 1)
+        self.assertEqual(calcut(4, 1, 1), 2)
 
 if __name__ == "__main__":
     unittest.main()
